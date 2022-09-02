3e00ab516905ae61e665db90926e9271c3ca1304
rajarameshm <rajaramesh.m@gmail.com>
2022-09-03 00:05:43 +0530

    first java file
    first java file hello world example

---
+++ Test.java
@@ -0,0 +1,5 @@
+public class Test { //class decleration
+    public static void main(String[] args) { //method decleration
+        System.out.println("Hello World !"); //statement/instruction
+    }
+}
