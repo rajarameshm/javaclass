151d317a7fcd792da47039bc05cc28c6d16ae0f2
rajarameshm <rajaramesh.m@gmail.com>
2022-09-04 05:41:50 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,5 +1,20 @@
-public class Test { //class decleration
-    public static void main(String[] args) { //method decleration
-        System.out.println("Hello World !"); //statement/instruction
+public class Test { //class declaration
+    //variable name - starts with small, no special char, camel case, case sensitive
+    public static String classVariable = "i am class level variable";
+    String instanceVariable = "i am an instance variable";
+    //Naming convention - name of the constructor same as its class name; no return type; can not be abstract, static, final, synchronized
+    //Naming convention - name of the constructor same as its class name; no return type; can not be abstract, static, final, synchronized
+    public Test() { //constructor
+    }
+
+    public static void main(String[] args) { //method declaration
+        System.out.println("Hello World !"); //statements/instructions
+        String localVariable = "i am a local variable";
+        System.out.println("this is local variable " + localVariable); //statements/instructions
+        //new Circle(2).area();
+        //new Rectangle(2, 3).area();
+        //new Square(5).area();
+        //Shape shape = new Circle(3.5f);
+        //shape.area();
     }
 }
